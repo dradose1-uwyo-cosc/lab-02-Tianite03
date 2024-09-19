eeb984f1fc86603c315352ffa588bcc200735852
Tianite03 <tblueme1@uwyo.edu>
2024-09-19 15:21:14 -0600

    Update cosc1010-lab02.py

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -2,8 +2,8 @@
 # UWYO COSC 1010
 # 9/19/24
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 12
+# Sources, people worked with, help given to: N/A
 # your
 # comments
 # here
@@ -18,30 +18,30 @@ print("Hello, COSC1010")
 hello_message = "Hello, COSC1010"
 print(hello_message)
 
-# Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+cowboy = "cowboy joe"
+print(cowboy.title)
 
 # Complete the following f-string print message 
-    # You will need to create your own variables and insert them  
-    # the final message should read `The University of Wyoming was founded in 1886`
-
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+    UW = "University of Wyoming"
+    date = "1886."
+print(f"The {UW} was founded in {date}")
 
 # Now let's do some math with variables 
-    # Create two variables x and y and assign them the values 5 and 10 respectively 
+   varX = 5
+    varY = 10
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+print(f"x + y = {varX + varY}")
+print(f"x - y = {varX - varY}")
+print(f"x * y = {varX * varY}")
+print(f"x / y = {varX / varY}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
     # String concatenation combines two strings together
     # It is done using the + operator
     # Create three variables:
-        # first_name, which is your first name 
-        # last_name, which is your last name
-        # space, which is a space character 
-    # Use string concatenation to print out your full name 
+        first_name = "Talon"
+        last_name = "Bluemel"
+        space = " "
+    print ("first_name" + "space" + "last_name")
